d4ca2ed721fff33c3572ce6fd64d78c7e2b27f6b
Reed Hambrook <reed.hambrook@gmail.com>
2022-04-23 17:33:55 -0700

    inital commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import webExtension from "vite-plugin-web-extension";
+import path from 'path';
+
+export default defineConfig({
+  root: "src",
+  // Configure our outputs - nothing special, this is normal vite config
+  plugins: [
+    solidPlugin(),
+    webExtension({
+        manifest: path.resolve(__dirname, "src/manifest.json"),
+        assets: "assets",
+        webExtConfig: {
+          firefox: "firefox",
+        },
+    }),
+  ],
+  build: {
+    outDir: path.resolve(__dirname, "build"),
+    target: 'esnext',
+    polyfillDynamicImport: false,
+    emptyOutDir: true,
+  },
+});
